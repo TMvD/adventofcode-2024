f71f31771b149398b0b899ff406ace80f8d2f348
Thomas van Dam <thomasvandam@gmail.com>
2024-12-02 02:03:23 -0500

    Day 2

---
+++ adventofcode/day2.py
@@ -0,0 +1,48 @@
+def main():
+    with open("./data/data_day2.txt", "r", encoding="utf-8") as file:
+        data = file.read().splitlines()
+    reports = parse_data(data)
+
+    report_results = [check_report_safety(x) for x in reports]
+    print("Safe reports: ", count_safe_reports(report_results))
+    dampened_report_results = [check_dampened_report_safety(x) for x in reports]
+    print("Dampened safe reports: ", count_safe_reports(dampened_report_results))
+
+
+def parse_data(reports: list[str]) -> list[list[int]]:
+    return [[int(x) for x in report.split()] for report in reports]
+
+
+def check_report_safety(report: list[int]) -> bool:
+    increasing, decreasing = True, True
+
+    for i, x in enumerate(report[1:], start=1):
+        current, last = report[i], report[i - 1]
+        if current - last == 0 or abs(current - last) > 3:
+            return False
+        # increasing
+        if current > last:
+            decreasing = False
+        # decreasing
+        elif current < last:
+            increasing = False
+    return increasing or decreasing
+
+
+def check_dampened_report_safety(report: list[int]) -> bool:
+    if check_report_safety(report):
+        return True
+
+    for i, _ in enumerate(report):
+        report_copy = report.copy()
+        report_copy.pop(i)
+        if check_report_safety(report_copy):
+            return True
+    return False
+
+
+def count_safe_reports(report_results: list[bool]) -> int:
+    return report_results.count(True)
+
+
+main()
---
+++ tests/test_day2.py
@@ -0,0 +1,44 @@
+import pytest
+
+from adventofcode.day2 import (
+    check_dampened_report_safety,
+    check_report_safety,
+    count_safe_reports,
+)
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        [[7, 6, 4, 2, 1], True],
+        [[1, 2, 7, 8, 9], False],
+        [[9, 7, 6, 2, 1], False],
+        [[1, 3, 2, 4, 5], False],
+        [[8, 6, 4, 4, 1], False],
+        [[1, 3, 6, 7, 9], True],
+    ],
+)
+def test_check_report_safety(input, expected):
+    # True == safe
+    # False == unsafe
+    assert check_report_safety(input) == expected
+
+
+def test_count_safe_reports():
+    reports = [True, False, False, False, True]
+    assert count_safe_reports(reports) == 2
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        [[7, 6, 4, 2, 1], True],
+        [[1, 2, 7, 8, 9], False],
+        [[9, 7, 6, 2, 1], False],
+        [[1, 3, 2, 4, 5], True],
+        [[8, 6, 4, 4, 1], True],
+        [[1, 3, 6, 7, 9], True],
+    ],
+)
+def test_check_dampened_report_safety(input, expected):
+    assert check_dampened_report_safety(input) == expected
