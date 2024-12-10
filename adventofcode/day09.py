840a8eff23b0075e8743e93cdee2f7390c4a66ad
Thomas van Dam <thomasvandam@gmail.com>
2024-12-10 01:54:55 -0500

    Day 9

--- adventofcode/day09.py
+++ adventofcode/day09.py
@@ -1,3 +1,6 @@
+import operator
+
+
 def main():
     with open("./data/data_day09.txt", "r", encoding="utf-8") as file:
         map = file.read()
@@ -6,6 +9,11 @@ def main():
     blocks = defragment(blocks)
     checksum = calculate_checksum(blocks)
     print("Checksum:", checksum)
+
+    blocks = decode_diskmap(map)
+    blocks = defragment_whole_files(blocks)
+    checksum = calculate_checksum(blocks)
+    print("Whole file checksum", checksum)
 
 
 def decode_diskmap(diskmap: str):
@@ -43,9 +51,61 @@ def calculate_checksum(blocks: list[str]):
     checksum = 0
     for position, file_id in enumerate(blocks):
         if file_id == ".":
-            break
+            continue
         checksum += position * int(file_id)
     return checksum
+
+
+def defragment_whole_files(blocks: list[str]):
+    file_id = find_max_file_id(blocks)
+
+    for file_id in range(file_id, -1, -1):
+        blocks = defragment_file(blocks, file_id)
+    return blocks
+
+
+def defragment_file(blocks: list[str], file_id: int):
+    file_start = find_start(blocks, str(file_id))
+    file_end = find_end(blocks, str(file_id))
+    file_size = file_end - file_start
+    empty_blocks = 0
+
+    for i, block in enumerate(blocks):
+        if block == ".":
+            empty_blocks += 1
+        else:
+            empty_blocks = 0
+
+        if i > file_start:
+            break
+
+        if file_size <= empty_blocks:
+            empty_start = i - empty_blocks + 1
+            empty_end = empty_start + file_size
+
+            empty_slice = slice(empty_start, empty_end)
+            file_slice = slice(file_start, file_end)
+            blocks[empty_slice], blocks[file_slice] = (
+                blocks[file_slice],
+                blocks[empty_slice],
+            )
+            break
+    return blocks
+
+
+def find_max_file_id(blocks: list[str]) -> int:
+    for file_id in reversed(blocks):
+        if file_id != ".":
+            return int(file_id)
+    raise ValueError
+
+
+def find_start(blocks: list[str], file_id: str):
+    return blocks.index(str(file_id))
+
+
+def find_end(blocks: list[str], file_id: str):
+    return len(blocks) - operator.indexOf(reversed(blocks), file_id)
 
 
 if __name__ == "__main__":
--- tests/test_day09.py
+++ tests/test_day09.py
@@ -4,6 +4,8 @@ from adventofcode.day09 import (
     calculate_checksum,
     decode_diskmap,
     defragment,
+    defragment_file,
+    defragment_whole_files,
     find_last_block_index,
 )
 
@@ -46,7 +48,44 @@ def test_find_last_block_index(blocks, expected):
     assert find_last_block_index(blocks) == expected
 
 
-def test_calculate_checksum():
-    assert (
-        calculate_checksum(list("0099811188827773336446555566..............")) == 1928
-    )
+@pytest.mark.parametrize(
+    "blocks, expected",
+    [
+        (
+            list("00...111...2...333.44.5555.6666.777.888899"),
+            list("00992111777.44.333....5555.6666.....8888.."),
+        )
+    ],
+)
+def test_defragment_whole(blocks, expected):
+    assert defragment_whole_files(blocks) == expected
+
+
+@pytest.mark.parametrize(
+    "blocks, file_id, expected",
+    [
+        (
+            list("00...111...2...333.44.5555.6666.777.888899"),
+            9,
+            list("0099.111...2...333.44.5555.6666.777.8888.."),
+        ),
+        (
+            list("0099.111777244.333....5555.6666.....8888.."),
+            2,
+            list("00992111777.44.333....5555.6666.....8888.."),
+        ),
+    ],
+)
+def test_defragment_file(blocks, file_id, expected):
+    assert defragment_file(blocks, file_id) == expected
+
+
+@pytest.mark.parametrize(
+    "blocks, expected",
+    [
+        (list("0099811188827773336446555566.............."), 1928),
+        (list("00992111777.44.333....5555.6666.....8888.."), 2858),
+    ],
+)
+def test_calculate_checksum(blocks, expected):
+    assert calculate_checksum(blocks) == expected
